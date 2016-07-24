ca313c77ea0787b4bb66c18a52316f96f92b95c9
Sally Hui <sallyhui97@gmail.com>
2016-07-23 22:51:35 -0400

    formatting, problem 5

---
+++ 005.py
@@ -0,0 +1,10 @@
+#2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+#What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+
+from sys import argv
+import math, time
+fileName, inputNum = argv
+
+tStart = time.time()
+print "yes"
+print (str((time.time() - tStart)))
